1c14ad1a30cd70b7fb5753a6140769da0361e536
drishyaisme <bhujel989@gmail.com>
2022-11-29 08:41:31 +0545

    changes

---
+++ index.js
@@ -0,0 +1,37 @@
+// let userName = "Sunil Bhujel";
+// // userName = 'drishya'
+
+// const age = 90;
+// const islogin = true;
+// const isregister = false;
+// let image = null;
+
+// // console.log(typeof(userName));
+
+// const person = {
+//     name: ' Sunil ',
+//     age: 30
+// };
+
+// const number = [22,34,55, 77, 99]
+
+// function addSome(){
+//     console.log('hello world');
+// }
+
+// const some = () => {
+//     console.log('1');
+// }
+
+// some();
+// addSome();
+
+let a = 90;
+const b = 100;
+a += 100;
+
+console.log(a);
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
