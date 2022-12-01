0bd06420d214716e3e15a0f9cc24ed446ba45f26
drishyaisme <bhujel989@gmail.com>
2022-12-01 09:52:18 +0545

    some javascript

--- index.js
+++ index.js
@@ -101,7 +101,101 @@
 // }
 // greet();
 
-const greet = (name, age, greetType) => {
-    console.log(`${name} ${greetType} ${age}`);
-}
-greet('Sunil', 90, 'hello')
+// const greet = (name, age, greetType) => {
+//     console.log(`${name} ${greetType} ${age}`);
+// }
+// greet('Sunil', 90, 'hello')
+
+
+
+// setTimeout = (() => {
+//     console.log("Hello world!");
+
+// }, 3);
+
+
+// setInterval(() => {
+//     const date = new Date();
+//     console.log(`${date.getHours()} ${date.getMinutes()} ${date,getSeconds()}`);
+// }, 1000)
+
+
+// let total = 0;
+// const numbers = [22, 55, 77, 99];
+// numbers.forEach((n) => {
+//     total = total + n;
+// });
+// console.log(total);
+
+
+// numbers.push(90);
+// numbers.pop();
+// var num = numbers.slice(1, 3);
+// console.log(num);
+
+
+// const data = [
+//     {
+//       "userId": 1,
+//       "id": 1,
+//       "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+//       "gender": "male"
+//     },
+//     {
+//       "userId": 1,
+//       "id": 2,
+//       "title": "qui est esse",
+//       "gender": "male"
+//     },
+//     {
+//       "userId": 1,
+//       "id": 3,
+//       "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+//       "gender": "female"
+//     }
+//   ];
+
+//   const newData = data.filter((m) => {
+//     return m.gender = 'male';
+//   });
+//   console.log(newData);
+
+
+//   const newData1 = data.filter((m) => {
+//     return m.id == 1;
+//   });
+//   console.log(newData1);
+
+
+// const dataMap = data.map((d) => {
+//     return d.title;
+
+// });
+// console.log(dataMap);
+// console.log(newNum);
+
+
+//   const numbers = [22, 55, 77, 99, 100];
+
+//   const newNum = numbers.map((n) => {
+//     return n * 2;
+//   });
+//   const sumOf = numbers.reduce((p, n) => {
+//     // 22 55
+//     // 77 77
+//     // 154 99
+//     return p + n;
+//   });
+//   console.log(sumOf);
+
+
+const person = {
+    name: 'sunil',
+    age: 29,
+};
+
+const {name, age} = person;
+console.log(age);
+const dataArray = ['hari', 'shyam'];
+const [mi, to] =  dataArray;
+console.log(mi);
